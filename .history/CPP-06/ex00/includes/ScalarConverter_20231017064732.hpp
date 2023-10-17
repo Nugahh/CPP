f93336cfe61b664e18c866fe0bd6fa12c93b47f1
nugahh <nugah@DESKTOP-96KGKIL.localdomain>
2023-10-17 06:48:24 +0200

    add cpp06

---
+++ .history/CPP-06/ex00/includes/ScalarConverter_20231017064732.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fwong <fwong@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/17 06:45:05 by fwong             #+#    #+#             */
+/*   Updated: 2023/10/17 06:45:05 by fwong            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCALARCONVERTER_HPP
+#define SCALARCONVERTER_HPP
+
+#include <iostream>
+
+class ScalarConverter {
+	public:
+		ScalarConverter();
+		ScalarConverter(const ScalarConverter &copy);
+		~ScalarConverter();	
+
+		ScalarConverter &operator=(const ScalarConverter &rhs);
+
+		void	convert(std::string str);
+};
+
+#endif
